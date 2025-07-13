a6529e5b6ec0c5ab5b3d53460d88134fa0261670
品川　和輝 <84860391+kazuki47@users.noreply.github.com>
2025-07-13 14:43:50 +0900

    Update time.tsx

--- pages/pomodoro_timer/time.tsx
+++ pages/pomodoro_timer/time.tsx
@@ -137,7 +137,6 @@ const StudyTimeDisplay = () => {
                       className="bg-blue-500 rounded-t-md transition-all duration-300 hover:bg-blue-600 relative group"
                       style={{ height: `${height}%`, minHeight: '4px' }}
                     >
-                      {/* ツールチップ */}
                       <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 opacity-0 group-hover:opacity-100 transition-opacity bg-gray-800 text-white text-xs rounded py-1 px-2 whitespace-nowrap">
                         {formatTime(item.totalTime)}
                       </div>
